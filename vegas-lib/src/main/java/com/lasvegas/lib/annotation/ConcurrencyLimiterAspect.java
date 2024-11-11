7f7224624680ad11fcaf1d7e6f34c67b066a926f
ahsimon <ahsimon.us@gmail.com>
2024-11-11 01:11:35 -0300

    add limiter-registry

--- vegas-lib/src/main/java/com/lasvegas/lib/annotation/ConcurrencyLimiterAspect.java
+++ vegas-lib/src/main/java/com/lasvegas/lib/annotation/ConcurrencyLimiterAspect.java
@@ -1,6 +1,7 @@
 package com.lasvegas.lib.annotation;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import com.netflix.concurrency.limits.Limiter;
@@ -16,36 +17,22 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 
 import java.lang.reflect.Method;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+
 
 @Aspect
 @Component
 public class ConcurrencyLimiterAspect {
+    private final LimiterRegistry limiterRegistry;
 
-    private final ConcurrentMap<String, Limiter<Void>> limiters = new ConcurrentHashMap<>();
+    @Autowired
+    public ConcurrencyLimiterAspect(LimiterRegistry limiterRegistry) {
+        this.limiterRegistry = limiterRegistry;
+    }
 
-    // Access your configuration values here
-    @Value("${concurrency.limits.processRequestLimiter.initialLimit:100}")
-    private int processRequestLimiterInitialLimit;
 
-    @Value("${concurrency.limits.anotherLimiter.initialLimit:50}")
-    private int anotherLimiterInitialLimit;
-
-    @Around("@annotation(concurrencyLimited)")
     public Object limitConcurrency(ProceedingJoinPoint joinPoint, ConcurrencyLimited concurrencyLimited) {
         String key = !concurrencyLimited.name().isEmpty() ? concurrencyLimited.name() : joinPoint.getSignature().toShortString();
-
-
-
-        Limiter<Void> limiter = limiters.computeIfAbsent(key, k -> {
-            AbstractLimit vegasLimit = VegasLimit.newBuilder()
-                    .initialLimit(processRequestLimiterInitialLimit)
-                    .build();
-            return SimpleLimiter.newBuilder()
-                    .limit(vegasLimit)
-                    .build();
-        });
+        Limiter<Void> limiter = limiterRegistry.getLimiter(key);
 
         Limiter.Listener listener = limiter.acquire(null).orElse(null);
         if (listener != null) {
@@ -61,6 +48,9 @@ public class ConcurrencyLimiterAspect {
             return invokeFallback(joinPoint, concurrencyLimited.fallbackMethod());
         }
     }
+
+
+
 
     private Object invokeFallback(ProceedingJoinPoint joinPoint, String fallbackMethodName) {
         try {
---
+++ vegas-lib/src/main/java/com/lasvegas/lib/annotation/LimiterRegistry.java
@@ -0,0 +1,56 @@
+package com.lasvegas.lib.annotation;
+import com.netflix.concurrency.limits.Limit;
+import com.netflix.concurrency.limits.Limiter;
+import com.netflix.concurrency.limits.limit.AbstractLimit;
+import com.netflix.concurrency.limits.limit.VegasLimit;
+import com.netflix.concurrency.limits.limiter.SimpleLimiter;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Component
+public class LimiterRegistry {
+
+    private final ConcurrentMap<String, Limiter<Void>> limiters = new ConcurrentHashMap<>();
+
+    private final ConcurrencyLimitProperties properties;
+
+    @Autowired
+    public LimiterRegistry(ConcurrencyLimitProperties properties) {
+        this.properties = properties;
+    }
+
+    @PostConstruct
+    public void initialize() {
+        for (Map.Entry<String, Integer> entry : properties.getLimiters().entrySet()) {
+            registerLimiter(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public void registerLimiter(String name, int initialLimit) {
+        AbstractLimit vegasLimit = VegasLimit.newBuilder()
+                .initialLimit(initialLimit)
+                .build();
+        limiters.put(name,  SimpleLimiter.newBuilder()
+                .limit(vegasLimit)
+                .build());
+    }
+
+    public Limiter<Void> getLimiter(String name) {
+        return limiters.getOrDefault(name, createDefaultLimiter());
+    }
+
+    private Limiter<Void> createDefaultLimiter() {
+        Limit vegasLimit= VegasLimit.newBuilder()
+                .initialLimit(properties.getDefaultLimit().getInitialLimit())
+                .build();
+
+      return   SimpleLimiter.newBuilder()
+                .limit(vegasLimit)
+                .build();
+    }
+}
